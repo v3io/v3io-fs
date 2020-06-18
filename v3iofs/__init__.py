eac0c8ed51f474d19ee10a0167c4a09c46a206a9
Gregory Hayes <gregory.hayes@ecolab.com>
2020-06-18 04:09:58 +0000

    Registry v3io for fsspec >0.6, <0.7.4

--- v3iofs/__init__.py
+++ v3iofs/__init__.py
@@ -27,5 +27,10 @@ from .file import V3ioFile  # noqa: F401
 from .fs import V3ioFS  # noqa: F401
 
 # TODO: Not sure about clobber=True
-fsspec.register_implementation('v3io', V3ioFS, clobber=True)
-del fsspec  # clear the module namespace
+try:
+    fsspec.register_implementation('v3io', V3ioFS, clobber=True)
+    del fsspec  # clear the module namespace
+except:
+    from fsspec.registry import known_implementations
+    known_implementations["v3io"] = {"class": "v3iofs.V3ioFS",
+                                    'err': 'Please install v3iofs to use the v3io fileysstem class'}
