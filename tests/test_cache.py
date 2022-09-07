24f9ab02a5b15f3208475090c527e3b537abde6c
Gal Topper <galt@iguazio.com>
2022-09-07 18:15:13 +0800

    Add puts to same key

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -14,8 +14,9 @@ def test_put_and_get():
 def test_invalidation_and_gc():
     cache = _Cache(3, 0)
     cache.put("k1", "v1")
-    cache.put("k2", "v1")
-    cache.put("k3", "v1")
+    cache.put("k2", "v2")
+    cache.put("k3", "v3.1")
+    cache.put("k3", "v3.2")
     assert cache.get("k1") is None
     assert cache.get("k2") is None
     assert cache.get("k3") is None
