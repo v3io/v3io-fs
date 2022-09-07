dcaa98a616bb7ece07160c54dcda4e7af5ec5d20
Gal Topper <galt@iguazio.com>
2022-09-07 18:59:28 +0800

    Improve tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -2,17 +2,18 @@ from v3iofs.fs import _Cache
 
 
 def test_put_and_get():
-    cache = _Cache(3, 100)
+    cache = _Cache(10, 100)
     cache.put("k1", "v1")
     cache.put("k2", "v2")
-    cache.put("k3", "v3")
+    cache.put("k3", "v3.1")
+    cache.put("k3", "v3.2")
     assert cache.get("k1") == "v1"
     assert cache.get("k2") == "v2"
-    assert cache.get("k3") == "v3"
+    assert cache.get("k3") == "v3.2"
 
 
 def test_invalidation_and_gc():
-    cache = _Cache(3, 0)
+    cache = _Cache(10, 0)
     cache.put("k1", "v1")
     cache.put("k2", "v2")
     cache.put("k3", "v3.1")
