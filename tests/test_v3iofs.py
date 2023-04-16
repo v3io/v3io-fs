390ef4e82b8029ef942fb0f336a3f758c53dcd02
Gal Topper <galt@iguazio.com>
2023-04-16 13:32:14 +0800

    Revert "Delete tests"
    This reverts commit df06a7b7399272d27fad0c30765facb938c40146.

---
+++ tests/test_v3iofs.py
@@ -0,0 +1,25 @@
+# Copyright 2020 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import fsspec
+
+from v3iofs import V3ioFS
+
+
+def test_register():
+    cls = fsspec.get_filesystem_class(V3ioFS.protocol)
+    assert cls is V3ioFS, "not registered"
+
+    fs = fsspec.filesystem("v3io")
+    assert isinstance(fs, V3ioFS), f"bad object class - {fs.__class__}"
